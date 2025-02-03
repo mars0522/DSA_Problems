57d12655dd562a20d76f02ceec5c6b8559a24c16
varun <varun.singh01@indiamart.com>
2025-02-03 09:33:23 +0530

    some basic recursion problem added

--- Recursion/basic_recursion.cpp
+++ Recursion/basic_recursion.cpp
@@ -43,6 +43,30 @@ int LogPower(int x, int n) {
     if (n%2==0) return temp * temp;
     return x*temp*temp;
 }
+void Towerofhanoi(int n, char A , char B , char C) {
+    if (n==0) return;
+    Towerofhanoi(n-1,A,C,B);
+    cout<<"Move disk "<<n<<" from "<<A<<" to "<<B<<endl;
+    Towerofhanoi(n-1,C,B,A);
+
+}
+
+void DisplayArray(int *arr , int n , int index) {
+    if (index >= n) return ;
+    cout<<arr[index]<<" ";
+    DisplayArray(arr,n,index+1);
+}
+void DisplayArrayInReverse(int *arr, int n ,int index) {
+    if (index >= n) return ;
+    DisplayArrayInReverse(arr,n,index+1);
+    cout<<arr[index]<<" ";
+}
+
+int MaxInArray(int *arr, int n, int index) {
+    if (index >= n) return INT16_MIN;
+    int max = MaxInArray(arr,n,index+1);
+    return max > arr[index]? max:arr[index];
+}
 int main() {
    #ifndef ONLINE_JUDGE
 	freopen("../input.txt","r",stdin);
@@ -52,8 +76,13 @@ int main() {
     // PrintDecreasingIncreasingOrder(5);
     // cout<<Factorial(5)<<endl;
     // cout<<Power(2,10)<<endl;
-    cout<<LogPower(2,12)<<endl;
-    return 0;
-    
+    // cout<<LogPower(2,12)<<endl;
+    // Towerofhanoi(5,'A','B','C');
+   int arr[] = {82,20,34,14,76,22};
+   int n = sizeof(arr)/sizeof(arr[0]);
+    // DisplayArray(arr,n,0);
+    // DisplayArrayInReverse(arr,n,0);
+    cout<<MaxInArray(arr,n,0);
+    return 0;  
 }
 
